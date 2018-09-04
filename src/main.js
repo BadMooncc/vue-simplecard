915d9ed097f12b03d9b0640066cae647793b721e
zhaodenghui <zhaodenghui@iyunxiao.com>
2018-09-04 18:30:49 +0800

    add axios options

--- src/main.js
+++ src/main.js
@@ -5,28 +5,28 @@ import App from './App'
 import router from './router'
 import axios from 'axios';
 import store from './store/';
+import axiosConfig from './config/axiosConfig'
 import rem from './assets/js/rem.js';
-console.log(rem);
-console.log(store);
 import '@/assets/js/common.js';
 
+Vue.use(axiosConfig);
 
 Vue.config.productionTip = false;
-Vue.prototype.$http=axios;
+Vue.prototype.$http = axios;
 // console.log(router) 
-//axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
+// axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
 
-axios.interceptors.request.use(function (config) {  //配置发送请求的信息
+axios.interceptors.request.use(function (config) {  // 配置发送请求的信息
   store.dispatch('loadShow');  
   return config;
 }, function (error) {
   return Promise.reject(error);
 });
 
-axios.interceptors.response.use(function (response) { //配置请求回来的信息
-  setTimeout(function(){
+axios.interceptors.response.use(function (response) { // 配置请求回来的信息
+  setTimeout(function (){
     store.dispatch('loadHide');
-  },800)
+  }, 800)
   
   return response;
 }, function (error) {
@@ -36,17 +36,17 @@ axios.interceptors.response.use(function (response) { //配置请求回来的信
 
 Vue.directive('demo', {
   bind: function (el, binding, vnode) {
-    //console.log(binding);
+    // console.log(binding);
   }
 });
-Vue.directive('demo2', function(el,binding){
-  //console.log(binding,'demo2');
+Vue.directive('demo2', function (el, binding){
+  // console.log(binding,'demo2');
 });
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
-  template: '<App/>',
-  components: { App }
+  components: { App },
+  template: '<App/>'
 })
