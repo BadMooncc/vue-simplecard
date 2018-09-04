2c420e5b747918667f92ef34d8cb1d7c4b00069b
zhaodenghui <zhaodenghui@iyunxiao.com>
2018-09-04 18:40:06 +0800

    整合axios配置项

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import store from '../store/';
 const axiosConfig = {};
 axiosConfig.install = function (){
-  axios.defaults.baseURL = 'http://result.eolinker.com'
+  axios.defaults.baseURL = 'http://result.eolinker.com';
+  axios.interceptors.request.use(function (config) {  // 配置发送请求的信息
+    store.dispatch('loadShow');  
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+  axios.interceptors.response.use(function (response) { // 配置请求回来的信息
+    setTimeout(function (){
+      store.dispatch('loadHide');
+    }, 800)
+    return response;
+  }, function (error) {
+    return Promise.reject(error);
+  });
 }
 
 export default axiosConfig;
--- src/main.js
+++ src/main.js
@@ -9,39 +9,12 @@ import axiosConfig from './config/axiosConfig'
 import rem from './assets/js/rem.js';
 import '@/assets/js/common.js';
 
+// axios配置项
 Vue.use(axiosConfig);
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
-// console.log(router) 
-// axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
 
-axios.interceptors.request.use(function (config) {  // 配置发送请求的信息
-  store.dispatch('loadShow');  
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
-axios.interceptors.response.use(function (response) { // 配置请求回来的信息
-  setTimeout(function (){
-    store.dispatch('loadHide');
-  }, 800)
-  
-  return response;
-}, function (error) {
-
-  return Promise.reject(error);
-});
-
-Vue.directive('demo', {
-  bind: function (el, binding, vnode) {
-    // console.log(binding);
-  }
-});
-Vue.directive('demo2', function (el, binding){
-  // console.log(binding,'demo2');
-});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
