41527e096e01584146b650d5a6eb72908494a37d
zhaodh09 <zhaodh09@guahao.com>
2019-09-05 15:03:39 +0800

    axios配置全局拦截器

--- src/main.js
+++ src/main.js
@@ -8,29 +8,30 @@ import store from './store/';
 import axiosConfig from './config/axiosConfig';
 import rem from './assets/js/rem.js';
 import '@/assets/js/common.js';
+import axiosConf from '../config/axios'
 
 Vue.use(axiosConfig);
-
+axiosConf()
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 // console.log(router)
 // axios的一些配置，比如发送请求显示loading，请求回来loading消失之类的
 
-axios.interceptors.request.use(function (config) { // 配置发送请求的信息
-  store.dispatch('loadShow');
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+// axios.interceptors.request.use(function (config) { // 配置发送请求的信息
+//   store.dispatch('loadShow');
+//   return config;
+// }, function (error) {
+//   return Promise.reject(error);
+// });
 
-axios.interceptors.response.use(function (response) { // 配置请求回来的信息
-  setTimeout(function () {
-    this.$store.dispatch('loadHide');
-  }, 800);
-  return response;
-}, function (error) {
-  return Promise.reject(error);
-});
+// axios.interceptors.response.use((response) => { // 配置请求回来的信息
+//   setTimeout(() => {
+//     store.dispatch('loadHide');
+//   }, 800);
+//   return response;
+// }, function (error) {
+//   return Promise.reject(error);
+// });
 
 Vue.directive('demo', {
   bind: function (el, binding, vnode) {
